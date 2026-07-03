b7e38a59bc0eb5642dae839a77fe5a9b7de151b5
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2026-07-03 17:05:42 +0530

    Create udf.sql

---
+++ DAY-2 (PART-2)/LAB EXERCISE/udf.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION get_jira_issue(issue_key VARCHAR)
+RETURNS VARIANT
+LANGUAGE PYTHON
+RUNTIME_VERSION = 3.9
+HANDLER = 'get_issue'
+EXTERNAL_ACCESS_INTEGRATIONS = (jira_integration)
+PACKAGES = ('requests')
+SECRETS = ('cred' = jira_api_token)
+AS
+$$
+import _snowflake
+import requests
+
+def get_issue(issue_key):
+    credentials = _snowflake.get_username_password('cred')
+    url = f"https://hkshitesh.atlassian.net/rest/api/3/issue/{issue_key}"
+    response = requests.get(
+        url,
+        auth=(credentials.username, credentials.password),
+        headers={"Accept": "application/json"}
+    )
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": response.status_code, "message": response.text}
+$$;
